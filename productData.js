f603e879be984e78f3d5e25c9bd47cf61b0bc7e3
znkbnk <133157126+znkbnk@users.noreply.github.com>
2023-07-31 20:06:20 +0100

    Create productData.js

---
+++ productData.js
@@ -0,0 +1,13 @@
+import React, { useState } from 'react';
+
+// Sample product data
+const productsData = [
+  {
+    id: 1,
+    name: 'Product 1',
+    description: 'This is the first product.',
+    price: 10,
+    reviews: [],
+  },
+  // Add more products here...
+];
