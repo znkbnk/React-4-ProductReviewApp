93b62f409a82798a388b6a07e0bd00e15c9bf371
znkbnk <133157126+znkbnk@users.noreply.github.com>
2023-07-31 20:07:35 +0100

    Create reviewForm.js

---
+++ reviewForm.js
@@ -0,0 +1,52 @@
+// Step 9: Create the ReviewForm component
+
+jsx
+Copy code
+import React, { useState } from 'react';
+
+const ReviewForm = ({ onSubmit }) => {
+  // Step 10: Add form fields
+  const [rating, setRating] = useState(5);
+  const [comment, setComment] = useState('');
+
+  // Step 10: Create event handlers
+  const handleRatingChange = (event) => {
+    setRating(Number(event.target.value));
+  };
+
+  const handleCommentChange = (event) => {
+    setComment(event.target.value);
+  };
+
+  // Step 11: Implement the form submit
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    onSubmit(rating, comment);
+    setRating(5);
+    setComment('');
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <label>
+        Rating:
+        <select value={rating} onChange={handleRatingChange}>
+          <option value="1">1</option>
+          <option value="2">2</option>
+          <option value="3">3</option>
+          <option value="4">4</option>
+          <option value="5">5</option>
+        </select>
+      </label>
+      <br />
+      <label>
+        Comment:
+        <textarea value={comment} onChange={handleCommentChange} />
+      </label>
+      <br />
+      <button type="submit">Submit Review</button>
+    </form>
+  );
+};
+
+export default ReviewForm;
