57c302687ccaa3275cda8cc4d460552df06ee965
znkbnk <133157126+znkbnk@users.noreply.github.com>
2023-07-31 20:08:37 +0100

    Create App.js

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css'; // Assuming you have a CSS file (e.g., App.css) for styling
+import ProductReviewApp from './ProductReviewApp';
+
+function App() {
+  return (
+    <div className="App">
+      <ProductReviewApp />
+    </div>
+  );
+}
+
+export default App;
