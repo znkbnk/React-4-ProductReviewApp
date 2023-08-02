d4bb46191278ae8decdf30f998af40a959625f36
znkbnk <133157126+znkbnk@users.noreply.github.com>
2023-08-02 19:13:58 +0100

    Update productData.js

--- productData.js
+++ productData.js
@@ -1,13 +1,15 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 // Sample product data
 const productsData = [
   {
     id: 1,
-    name: 'Product 1',
-    description: 'This is the first product.',
+    name: "Product 1",
+    description: "This is the first product.",
     price: 10,
     reviews: [],
   },
   // Add more products here...
 ];
+
+export default productsData;
